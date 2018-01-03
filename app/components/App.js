b87fb8edaade5494df0594ff701d92c0556c3b89
Ryan Allred <ryanallred@Ryans-MacBook-Pro.local>
2018-01-03 02:13:57 -0700

    finished app not buit for production

--- app/components/App.js
+++ app/components/App.js
@@ -7,6 +7,7 @@ var Switch = ReactRouter.Switch;
 var Nav = require('./Nav');
 var Home = require('./Home');
 var Battle = require('./Battle');
+var Results = require('./Results');
 
 
 
@@ -18,8 +19,9 @@ class App extends React.Component {
           <Nav />
           <Switch>
             <Route exact path='/' component={Home} />
-            <Route exact path='/popular' component={Popular} />
-            <Route path='/battle' component={Battle} />
+            <Route exact path='/battle' component={Battle} />
+            <Route path='/battle/results' component={Results}/>
+            <Route path='/popular' component={Popular} />
             <Route render={function (){
               return <p>Not Found</p>
             }} />
--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,8 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var Loading = require('./Loading')
 var api = require('../utils/api');
+
 
 function SelectLanguage (props) {
   var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -93,7 +95,7 @@ class Popular extends React.Component {
           onSelect={this.updateLanguage}
         />
         {!this.state.repos
-          ? <p>LOADING</p>
+          ? <Loading />
           : <RepoGrid repos={this.state.repos } />}
       </div>
     )
